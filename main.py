9420e4ed57ebfc88d66b6185421bffc707af3129
Omar Einea <eineao@gmail.com>
2018-03-01 14:40:20 +0400

    Made TestButton show selected brand and product in results

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from layout import Ui_IoTTestbed
 class IoTTestbed(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        ui = Ui_IoTTestbed()
-        ui.setupUi(self)
+        self.ui = Ui_IoTTestbed()
+        self.ui.setupUi(self)
+        self.ui.testButton.clicked.connect(self.test_device)
+        self.ui.exitButton.triggered.connect(self.close)
+
+    def test_device(self):
+        results = "Testing the security of "
+        results += self.ui.brandsList.currentText() + " "
+        results += self.ui.productsList.currentText() + "..."
+        self.ui.resultsOutput.setText(results)
 
 
 if __name__ == "__main__":
