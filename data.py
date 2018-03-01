a28ac6a01767204c5c4c215ea98c568042cdb2d9
Omar Einea <eineao@gmail.com>
2018-03-01 19:38:48 +0400

    Placed data in a separate file & linked products with categories

---
+++ data.py
@@ -0,0 +1,13 @@
+categories = {
+    "Choose a category...": [],
+    "Smart sockets": [
+        "Wiwo S20 WiFi socket",
+        "Wiwo S25 WiFi socket",
+        "Wiwo S31 WiFi socket",
+    ],
+    "Thermostats": [
+        "Nest Thermostat E",
+        "Nest Thermostat 3rd",
+        "Nest Thermostat 2rd",
+    ]
+}
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
 from layout import Ui_IoTTestbed
+from data import categories
 
 
 class IoTTestbed(QtWidgets.QMainWindow):
@@ -7,14 +8,19 @@ class IoTTestbed(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = Ui_IoTTestbed()
         self.ui.setupUi(self)
+        self.ui.categoriesCombo.addItems(categories)
+        self.ui.categoriesCombo.currentTextChanged.connect(self.setProducts)
         self.ui.startTestingButton.clicked.connect(self.testDevice)
         self.ui.exitButton.triggered.connect(self.close)
 
+    def setProducts(self, category):
+        self.ui.productsCombo.clear()
+        self.ui.productsCombo.addItems(categories[category])
+
     def testDevice(self):
-        results = "Testing the security of "
-        results += self.ui.categoriesCombo.currentText() + " "
-        results += self.ui.productsCombo.currentText() + "..."
-        self.ui.resultsTextArea.setText(results)
+        product = self.ui.productsCombo.currentText()
+        if product:
+            self.ui.resultsTextArea.setText("Testing the security of " + product)
 
 
 if __name__ == "__main__":
