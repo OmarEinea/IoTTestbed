21859ca3cee26319561fb6cbb91d4b6d165d7caa
Omar Einea <eineao@gmail.com>
2018-03-02 17:31:50 +0400

    Made UI elements accessible from 'self' And Shortened Imports

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from layout import Ui_IoTTestbed
 from execute import Execute
 from data import categories
 
 
-class IoTTestbed(QtWidgets.QMainWindow):
+class IoTTestbed(QMainWindow, Ui_IoTTestbed):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_IoTTestbed()
-        self.ui.setupUi(self)
-        self.ui.categoriesCombo.addItems(categories)
-        self.ui.categoriesCombo.currentTextChanged.connect(self.setProducts)
-        self.ui.startTestingButton.clicked.connect(self.testDevice)
-        self.ui.exitButton.triggered.connect(self.close)
+        self.setupUi(self)
+        self.categoriesCombo.addItems(categories)
+        self.categoriesCombo.currentTextChanged.connect(self.setProducts)
+        self.startTestingButton.clicked.connect(self.testDevice)
+        self.exitButton.triggered.connect(self.close)
 
     def setProducts(self, category):
-        self.ui.productsCombo.clear()
-        self.ui.productsCombo.addItems(categories[category])
+        self.productsCombo.clear()
+        self.productsCombo.addItems(categories[category])
 
     def testDevice(self):
-        product = self.ui.productsCombo.currentText()
+        product = self.productsCombo.currentText()
         if not product: return
-        self.ui.resultsTextArea.clear()
-        self.ui.resultsTextArea.append("Testing the security of " + product + "...\n")
+        self.resultsTextArea.clear()
+        self.resultsTextArea.append("Testing the security of " + product + "...\n")
         self.test = Execute("test.py")
-        self.test.progressed.connect(self.ui.resultsTextArea.append)
+        self.test.progressed.connect(self.resultsTextArea.append)
         self.test.start()
 
 
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     main = IoTTestbed()
     main.show()
     sys.exit(app.exec_())
