6db925bcdb80bc989ae9a3ac327927f373abc68a
Omar Einea <eineao@gmail.com>
2018-03-03 17:48:19 +0400

    Made testingButton change colors depending on execution state

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ from execute import Execute
 class IoTTestbed(QMainWindow, Ui_IoTTestbed):
     def __init__(self):
         super().__init__()
+        self.test = None
         self.setupUi(self)
         self.categoriesCombo.addItems(categories)
         self.categoriesCombo.currentTextChanged.connect(self.setProducts)
         self.productsCombo.currentTextChanged.connect(self.setTests)
-        self.startTestingButton.clicked.connect(self.startTesting)
+        self.testsListCombo.itemSelectionChanged.connect(self.toggleTestingButton)
+        self.testingButton.clicked.connect(self.startTesting)
         self.exitButton.triggered.connect(self.close)
-        self.testsListCombo.currentTextChanged.connect(
-            lambda: self.startTestingButton.setEnabled(True)
-        )
 
     def setProducts(self, category):
         self.productsCombo.clear()
@@ -30,13 +29,13 @@ class IoTTestbed(QMainWindow, Ui_IoTTestbed):
         self.testsListCombo.clear()
         tests = categories[self.categoriesCombo.currentText()].get(product, False)
         self.testsListCombo.setEnabled(tests is not None)
-        self.startTestingButton.setEnabled(False)
         if tests:
             self.testsListCombo.addItems(tests)
         elif tests is None:
             self.testsListCombo.addItem("No tests available")
 
     def startTesting(self):
+        if self.test: return
         self.resultsTextArea.clear()
         test_name = self.testsListCombo.currentItem().text()
         script_path = scripts.get(test_name)
@@ -45,9 +44,23 @@ class IoTTestbed(QMainWindow, Ui_IoTTestbed):
             self.resultsTextArea.append(f'Running "{test_name}" script on {product}...\n')
             self.test = Execute("test.py")
             self.test.progressed.connect(self.resultsTextArea.append)
+            self.test.finished.connect(self.stopTesting)
             self.test.start()
+            self.testingButton.setText("Stop Testing")
+            self.testingButton.setStyleSheet("color: red")
         else:
             self.resultsTextArea.append(f'"{test_name}" script is not available yet!')
+
+    def stopTesting(self):
+        self.test = None
+        self.testingButton.setText("Start Testing")
+        self.toggleTestingButton()
+
+    def toggleTestingButton(self):
+        if self.test: return
+        enabled = len(self.testsListCombo.selectedItems()) > 0
+        self.testingButton.setEnabled(enabled)
+        self.testingButton.setStyleSheet("color:" + ["grey", "green"][enabled])
 
 
 if __name__ == "__main__":
