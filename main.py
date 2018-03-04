be27db4fb0e7ef05956d538ef5ebae4d6e4f877f
Omar Einea <eineao@gmail.com>
2018-03-04 19:37:30 +0400

    Replaced QThread with QProcess to allow execution interrupts

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QProcess
 from data import categories, scripts
 from layout import Ui_IoTTestbed
-from execute import Execute
 
 
 class IoTTestbed(QMainWindow, Ui_IoTTestbed):
     def __init__(self):
         super().__init__()
-        self.test = None
+        self.process = None
         self.setupUi(self)
         self.categoriesCombo.addItems(categories)
         self.categoriesCombo.currentTextChanged.connect(self.setProducts)
@@ -35,32 +35,40 @@ class IoTTestbed(QMainWindow, Ui_IoTTestbed):
             self.testsListCombo.addItem("No tests available")
 
     def startTesting(self):
-        if self.test: return
+        if self.process:
+            return self.stopTesting()
         self.resultsTextArea.clear()
         test_name = self.testsListCombo.currentItem().text()
         script_path = scripts.get(test_name)
         if script_path:
             product = self.productsCombo.currentText()
             self.resultsTextArea.append(f'Running "{test_name}" script on {product}...\n')
-            self.test = Execute("test.py")
-            self.test.progressed.connect(self.resultsTextArea.append)
-            self.test.finished.connect(self.stopTesting)
-            self.test.start()
+            self.process = QProcess()
+            self.process.setProcessChannelMode(QProcess.MergedChannels)
+            self.process.readyReadStandardOutput.connect(self.appendResults)
+            self.process.finished.connect(self.stopTesting)
+            self.process.start("python", ["-u", "tests/test.py"])
             self.testingButton.setText("Stop Testing")
             self.testingButton.setStyleSheet("color: red")
         else:
             self.resultsTextArea.append(f'"{test_name}" script is not available yet!')
 
     def stopTesting(self):
-        self.test = None
+        if self.process:
+            self.process = self.process.kill()
         self.testingButton.setText("Start Testing")
         self.toggleTestingButton()
 
     def toggleTestingButton(self):
-        if self.test: return
+        if self.process: return
         enabled = len(self.testsListCombo.selectedItems()) > 0
         self.testingButton.setEnabled(enabled)
         self.testingButton.setStyleSheet("color:" + ["grey", "green"][enabled])
+
+    def appendResults(self):
+        self.resultsTextArea.append(
+            str(self.process.readAllStandardOutput(), encoding="utf-8")
+        )
 
 
 if __name__ == "__main__":
